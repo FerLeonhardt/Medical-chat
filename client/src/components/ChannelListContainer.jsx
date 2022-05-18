12016e5ba881bfd7700d5bba621584d7c9254cf0
FerLeonhardt <fnleonhardt@gmail.com>
2022-05-18 12:25:51 -0400

    assets-setup

--- client/src/components/ChannelListContainer.jsx
+++ client/src/components/ChannelListContainer.jsx
@@ -3,12 +3,18 @@ import { ChannelList, useChatContext } from 'stream-chat-react';
 import {Cookies} from 'universal-cookie'
 
 import { ChannelSearch, TeamChannelList, TeamChannelPreview } from './index';
-
+import Logout from '../assets/'
 const sideBar= ()=> (
   <div className="channel-list__sideBar">
     <div className='channel-list__sideBar-icon1'>
       <div className='icon1-inner'>
-        <img src="{HospitalIcin" alt="hospital" width='30' />
+        <img src={HospitalIcon} alt="hospital" width='30' />
+
+      </div>
+    </div>
+    <div className='channel-list__sideBar-icon1'>
+      <div className='icon1-inner'>
+        <img src={LogoutIcon} alt="LogOut" width='30' />
 
       </div>
     </div>
